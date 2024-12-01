e67ae71330cc9ab6b393aa79d849e1f59484acc4
FacundoBacaicoa <163307452+FacundoBacaicoa@users.noreply.github.com>
2024-12-01 13:21:08 -0300

    Revert "Develop"

--- src/app/componentes/buscador/buscador.component.ts
+++ src/app/componentes/buscador/buscador.component.ts
@@ -10,21 +10,18 @@ export class BuscadorComponent {
 @Input() title: string='Buscar';// Título del input
 @Input() titlePlaceholder: string='';
 @Input() data: any[]=[]; // Lista de datos a buscar
-@Input() propiedades: string[]= []; // Propiedad del objeto para filtrar
+@Input() propiedad: string= ''; // Propiedad del objeto para filtrar
 
 @Output() filtrarData=new EventEmitter<any[]>(); // Emitir resultados filtrados
 
 
 buscarText: string='';// Texto ingresado por el usuario
 
-
-  busqueda() {
-    const filtrado = this.data.filter(item => 
-      this.propiedades.some(propiedad => 
-        item[propiedad]?.toLowerCase().includes(this.buscarText.toLowerCase())
-      )
-    );
-    this.filtrarData.emit(filtrado);
-  }
+busqueda(){
+const filtrado= this.data.filter((item)=>
+  item[this.propiedad].toLowerCase().includes(this.buscarText.toLowerCase())
+);
+this.filtrarData.emit(filtrado);
+}
 
 }
