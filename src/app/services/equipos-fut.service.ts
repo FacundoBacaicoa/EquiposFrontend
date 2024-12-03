cab9239418ee2d7b6ff482b054136da53cfba7f6
FacundoBacaicoa <facundobacaicoa@gmail.com>
2024-12-03 19:44:47 -0300

    Actualizacion agregar component y equiposFutServices

--- src/app/services/equipos-fut.service.ts
+++ src/app/services/equipos-fut.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { Equipo } from '../interfaces/equipo-interfaz';
 import { Jugador } from '../interfaces/jugador-interfaz';
 
@@ -25,11 +25,25 @@ getListEquipos(keyword: string): Observable<any> {
  }
 
  saveEquipo(equipo: Equipo): Observable<any>{
- return this.http.post(this.myAppUrl + this.myApiUrl, equipo);
+ return this.http.post(this.myAppUrl + this.myApiUrl, equipo).pipe(
+  catchError((error)=>{
+    if(error.status=== 404 && error.error.message === 'el equipo ya existe'){
+        return throwError('El equipo ya existe, no es posible agregarlo')
+    }
+    return throwError('Ocurrió un error al intentar guardar el equipo.');
+  })
+ );
  }
 
  updateEquipo(id_e:number,equipo:Equipo): Observable<any>{
-  return this.http.put(this.myAppUrl+ this.myApiUrl + id_e,equipo);
+  return this.http.put(this.myAppUrl+ this.myApiUrl + id_e,equipo).pipe(
+    catchError((error) => {
+      if (error.status === 404 && error.error.message === 'Ya existe otro equipo con el mismo nombre, país y ciudad') {
+        return throwError('Ya existe un equipo con los mismos datos, no es posible actualizarlo.');
+      }
+      return throwError('Ocurrió un error al intentar actualizar el equipo.');
+    })
+  );
  }
 
  getEquipoById(id: number): Observable<Equipo> {
